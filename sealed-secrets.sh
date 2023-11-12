62cc85e0c3d7a0e28d71d641ac2a9761a6ce5143
pblgllgs <pbl.gllgs@gmail.com>
2023-11-12 14:45:16 -0300

    change names, structure and updates

--- sealed-secrets.sh
+++ sealed-secrets.sh
@@ -3,8 +3,8 @@
 # exit when any command fails
 set -e
 
-rm environments/staging/my-app/users/secret.yml
-rm environments/staging/my-app/albums/secret.yml
+rm environments/staging/my-app/secret-albums.yaml
+rm environments/staging/my-app/secret-users.yaml
 
-kubeseal --controller-name sealed-secrets -o yaml -n default  < ignore/secrets-albums.yml > environments/staging/my-app/users/secret.yml
-kubeseal --controller-name sealed-secrets -o yaml -n default  < ignore/secrets-users.yml > environments/staging/my-app/albums/secret.yml
+kubeseal --controller-name sealed-secrets -o yaml -n default  < ignore/secrets-users.yml > environments/staging/my-app/secret-users.yaml
+kubeseal --controller-name sealed-secrets -o yaml -n default  < ignore/secrets-albums.yml > environments/staging/my-app/secret-albums.yaml
