03e5ae07e2cc230653513a6a99c5e78c798a094e
pblgllgs <pbl.gllgs@gmail.com>
2023-11-12 12:13:54 -0300

    change namespace

--- sealed-secrets.sh
+++ sealed-secrets.sh
@@ -5,5 +5,5 @@ set -e
 
 rm environments/staging/my-app/1-sealed-*
 
-kubeseal --controller-name sealed-secrets -o yaml -n staging  < ignore/1-secrets-albums.yml > environments/staging/my-app/1-sealed-secrets-albums.yml
-kubeseal --controller-name sealed-secrets -o yaml -n staging  < ignore/1-secrets-users.yml > environments/staging/my-app/1-sealed-secrets-users.yml
+kubeseal --controller-name sealed-secrets -o yaml -n default  < ignore/1-secrets-albums.yml > environments/staging/my-app/1-sealed-secrets-albums.yml
+kubeseal --controller-name sealed-secrets -o yaml -n default  < ignore/1-secrets-users.yml > environments/staging/my-app/1-sealed-secrets-users.yml
