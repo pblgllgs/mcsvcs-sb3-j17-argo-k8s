826bda6a2c72276b933f21323b0f2a04c1c50db4
pblgllgs <pbl.gllgs@gmail.com>
2023-11-12 11:26:53 -0300

    script generation sealed secrets

---
+++ sealed-secrets.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+rm environments/staging/my-app/1-sealed-*
+
+kubeseal --controller-name sealed-secrets -o yaml -n staging  < ignore/1-secrets-albums.yml > environments/staging/my-app/1-sealed-secrets-albums.yml
+kubeseal --controller-name sealed-secrets -o yaml -n staging  < ignore/1-secrets-users.yml > environments/staging/my-app/1-sealed-secrets-users.yml
